28f59ab794f7ffce0d203728a2649078def6413f
Seth Knoop <sethknoop@robots.et.byu.edu>
2025-06-09 17:01:02 -0600

    See previous message

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="clip_dinoiser",
     version="0.1",
-    packages=find_packages(),  # Automatically finds your Python packages
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "clip_dinoiser.configs": ["*.yaml"],
+    },
     install_requires=[
         # List dependencies here, or leave empty if you're managing them elsewhere
         "torch",
